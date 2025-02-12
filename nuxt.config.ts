0f9740ab41bf463ce2954563b98c79b508dd9e1b
Yamu_Studio <95989535+yamu-studio@users.noreply.github.com>
2025-02-12 11:36:19 +0900

    -add Topページ追加

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: [
+    '@fortawesome/fontawesome-free/css/all.css',
+    // 'bulma/css/bulma.css',
+    'bulma/css/versions/bulma-no-dark-mode.css',
+  ],
+})
